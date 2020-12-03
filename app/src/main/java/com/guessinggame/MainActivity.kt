e555b70bac74de7117ab3065a4d3eea99c852eb7
Pooja pokharel <pooja.pokhrel36@gmail.com>
2020-12-03 10:05:34 -0800

    final game

--- app/src/main/java/com/guessinggame/MainActivity.kt
+++ app/src/main/java/com/guessinggame/MainActivity.kt
@@ -4,46 +4,144 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import java.util.*
+import androidx.appcompat.app.AlertDialog
 import kotlin.random.Random
 import kotlin.random.nextInt
-import kotlin.time.seconds
 
 class MainActivity : AppCompatActivity() {
+     var count = 0
+    var right: Int? = 1
+    var wrong: Int? = 1
     lateinit var btnone: Button
     lateinit var btntwo: Button
     lateinit var totalscore: TextView
+    lateinit var totalscore2: TextView
+    lateinit var rightans: TextView
+    lateinit var wrongans: TextView
+    lateinit var resetgame: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//get a random number
+//get a random number app open
         val myrandom1 = Random.nextInt(10..100)
         val myrandom2 = Random.nextInt(10..100)
+
 
         btnone = findViewById(R.id.button1)
         btntwo = findViewById(R.id.button2)
         totalscore = findViewById(R.id.totalscore)
+        totalscore2 = findViewById(R.id.totalscore2)
+        rightans = findViewById(R.id.rightans)
+        wrongans = findViewById(R.id.wrongans)
+        resetgame = findViewById(R.id.resetgame)
         btnone.text = "$myrandom1"
         btntwo.text = "$myrandom2"
+        totalscore.text = "Invlaid!!"
+        rightans.text = "Total Right:0"
+        wrongans.text = "Total Wrong:0"
+
+        resetgame.setOnClickListener {
+            gameresetdone()
+
+            val a = Random.nextInt(10..100)
+            val b = Random.nextInt(10..100)
+            btntwo.text = ("$a")
+            btnone.text = ("$b")
+            rightans.text = "Total Right:0"
+            wrongans.text = "Total Wrong:0"
+            count = 0
+            right = 0
+            wrong = 0
+            totalscore.text = "Invlaid!!"
+            totalscore2.setText("Total click is: $count")
+        }
+
         btnone.setOnClickListener {
-            if (btntwo.text.toString() > btnone.text.toString())
-                totalscore.setText("you are right")
-            else (btntwo.text.toString() < btnone.text.toString())
-                totalscore.setText("you are wrong !!")
+
+            if (count == 10)
+                basicAlert()
+            else
+            {
+                val a = Random.nextInt(10..100)
+                val b = Random.nextInt(10..100)
+                count++
+
+                if (btntwo.text.toString() <= btnone.text.toString())
+                {
+                    totalscore.setText("Correct!!")
+                   val r = right?.plus(1)
+                    rightans.text = "Total Right:" + (right)
+                    right = (r)
+                }
+
+                else if (btntwo.text.toString() > btnone.text.toString())
+                {
+                    totalscore.setText("Wrong!!")
+                    val w = wrong?.plus(1)
+                    wrongans.text = "Total Wrong:" + (wrong)
+                    wrong = (w)
+
+                }
+                totalscore2.setText("Total Click is: $count")
+                btntwo.text = ("$a")
+                btnone.text = ("$b")
+            }
+
 
 
         }
         btntwo.setOnClickListener {
-            if (btnone.text.toString() < btntwo.text.toString())
-                totalscore.setText("you are right")
-            else (btnone.text.toString() > btntwo.text.toString())
-                totalscore.setText("you are wrong !!")
+            if (count == 10)
+                basicAlert()
+            else {
+                val a = Random.nextInt(10..100)
+                val b = Random.nextInt(10..100)
+                count++
+                if (btnone.text.toString() <= btntwo.text.toString())
+
+                {
+                    totalscore.setText("Correct!!")
+                   val r = right?.plus(1)
+                    rightans.text = "Total Right:" + (right)
+                    right = (r)
 
 
+                }
+
+               else if (btnone.text.toString() > btntwo.text.toString())
+                {
+                    totalscore.setText("Wrong!!")
+                    val w = wrong?.plus(1)
+                    wrongans.text = "Total Wrong:" + (wrong)
+                    wrong = (w)
+                }
+
+                totalscore2.setText("Total Click is: $count")
+                btntwo.text = ("$a")
+                btnone.text = ("$b")
+            }
         }
 
+    }
+    fun basicAlert() {
+        val builder = AlertDialog.Builder(this)
+        with(builder)
+        {
+            setTitle("You Can't Play more Game Please Reset Game")
+            setMessage("Total Click: $count ${rightans.text} ${wrongans.text}")
+            show()
+        }
+    }
+    fun gameresetdone() {
+        val builder = AlertDialog.Builder(this)
+        with(builder)
+        {
+            setTitle("Game Reset Sucess!!")
+            setMessage("Now you can Play New Game.")
+            show()
+        }
+    }
 
-}
 
 }
